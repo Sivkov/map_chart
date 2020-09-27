ae945e53722172200bd346d5be42064b50c949de
Alex <sivkov78@ya.ru>
2020-09-27 19:52:16 +0400

    Initial comit

---
+++ precache-manifest.a6ca04c8a236c54ad9fa1ff0dad86395.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe1e6f0059b6630735d819e78855ec2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c589bc48e5f516c4a0",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "96f4ba8d81516c0b9b3a",
+    "url": "/static/css/main.00a3a786.chunk.css"
+  },
+  {
+    "revision": "48c589bc48e5f516c4a0",
+    "url": "/static/js/2.4b2b003f.chunk.js"
+  },
+  {
+    "revision": "0fabf870503395047b753534c93ae6d0",
+    "url": "/static/js/2.4b2b003f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f4ba8d81516c0b9b3a",
+    "url": "/static/js/main.d2a64deb.chunk.js"
+  },
+  {
+    "revision": "f3296afc30b82c5249e8",
+    "url": "/static/js/runtime-main.79635c44.js"
+  }
+]);
